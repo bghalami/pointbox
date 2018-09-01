2d15b8648be1b0f33f20db83159b2a6a2810b9bd
bghalami <benjaminghalami@gmail.com>
2018-09-01 17:41:51 -0600

    created reward and user reward migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :user_rewards
+  has_many :rewards, through: :user_rewards
   has_secure_password
   validates :username, presence: true, uniqueness: true
 end
